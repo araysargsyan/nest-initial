f0dee22b9a0026a6d31824d6646efb21ba962099
araysargsyan <araysargsyan96@gmail.com>
2021-10-19 01:34:22 +0400

    fififiy

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,23 +1,6 @@
-import { Body, Controller, ForbiddenException, Get, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserEntity } from './user.entity';
 import { CreateUserDto } from './dto/createUser.dto';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { extname } from 'path';
-import { diskStorage } from 'multer';
-import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
-
-const validateFileType = (extt) => {
-    return (req, file, callback) => {
-        console.log(req, file, extt, 999);
-        const ext = extname(file.originalname);
-        if (ext !== extt) {
-            req.fileValidationError = 'Invalid file type';
-            return callback(new Error('Invalid file type'), false);
-        }
-        return callback(null, true);
-    };
-};
 
 @Controller('user')
 export class UserController {
